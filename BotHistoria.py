d17553d0003ec513c4463bed5754099d2b048cb7
Diego Neri <diego.holanda.neri@gmail.com>
2022-11-08 08:37:13 -0300

    Code update
    Using POO, the code has been updated.

--- BotHistoria.py
+++ BotHistoria.py
@@ -1,31 +1,58 @@
-# -*- coding: utf-8 -*-
+import os
 import tweepy
-import time
+from dotenv import load_dotenv
 
-auth = tweepy.OAuthHandler('consumer_key', 'consumer_secret')
-# consumer_key, consumer_secret
-# etapa de autenticação. Essas chaves são obtidas dentro do Twitter Deveolper, "API Key" e "API Key Secret"
+class Tweets:
+  """
+    This class will get the key values from twitter developer (https://developer.twitter.com/) 
+    and will call a method which show the user who tweeted: "a história vai cobrar". After that, the method
+    "__tweeting", using self.api.update_status, will answer with the follow phrase: 
+    " A história não cobra nada! Ustra morreu de velho."
 
-auth.set_access_token('key', 'secret')
-#key, secret
+    Tokens will not show for security reasons.
 
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-# esses dois parâmetros é só para executar depois que o limite dos tweets for atingido
+    ...
+    Methods
+    -------
+    No methods available.
+  """
+  
+  def __init__(self, searching_phrase: str, tweets_count: int):
+    
+    load_dotenv()
+    
+    consumer_key = os.getenv('consumer_key')
+    consumer_secret_key = os.getenv('consumer_secret_key')
+    access_token = os.getenv('access_token')
+    access_token_secret = os.getenv('access_token_secret')
 
-user = api.me()
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+    # consumer_key, consumer_secret
+    # Auth step. You can take both keys in https://developer.twitter.com/, "API Key" e "API Key Secret"
 
-print(user)
+    auth.set_access_token(access_token, access_token_secret)
+    #key, secret
 
-search = 'a história vai cobrar'
-numero = 30
+    self.api = tweepy.API(auth, wait_on_rate_limit=True)
+    # This line will be executed after que o limite dos tweets for atingido
 
-for tweet in tweepy.Cursor(api.search, search).items(numero):
-  try:
-    print("nome do usuário: @", tweet.user.screen_name)
-    api.update_status(status='@' + tweet.user.screen_name + " Ustra morreu de velho.")
-    print('Tweet enviado corretamente')
-    time.sleep(30)
-  except tweepy.TweepError as error:
-    print(error.reason)
-  except StopIteration:
-    break
+    self.searching_phrase = searching_phrase
+    self.tweets_count = tweets_count
+
+    self.__tweeting()
+
+  def __tweeting(self):
+    for tweet in tweepy.Cursor(self.api.search_tweets, self.searching_phrase).items(self.tweets_count):
+      try:
+        print("Username: @", tweet.user.screen_name)
+        self.api.update_status(
+          status='@' + tweet.user.screen_name + " A história não cobra nada! Ustra morreu de velho."
+        )
+        print('Tweet was sent')
+      except tweepy.TweepError as error:
+        print(error.reason)
+      except StopIteration:
+        break
+
+if __name__ == '__main__':
+  Tweets('A história vai cobrar', 1)
